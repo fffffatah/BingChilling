1943cd48bc1ab6692deec6213abfc4220e38264d
A.F.M. Noorullah <43249848+fffffatah@users.noreply.github.com>
2022-08-09 22:59:36 +0600

    set nosubscriber behaviour

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ const playdl = require('play-dl');
 const { createAudioPlayer, createAudioResource, joinVoiceChannel, NoSubscriberBehavior } = require('@discordjs/voice');
 const youtubesearchapi = require('youtube-search-api');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING", "GUILD_VOICE_STATES"] })
-const player = createAudioPlayer({
-    behaviors: {
-        noSubscriber: NoSubscriberBehavior.Stop
-    }
-});
+const player = createAudioPlayer();
 var musicQueue = [];
 var message = null;
 var connection = null;
